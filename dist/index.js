a3cd9037e6d37ee53339756d5d6d26f910b73623
jamesz96 <jamesz96.au@gmail.com>
2021-12-07 14:56:54 +1100

    Add dist to ignore

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getCountries = void 0;
-var countries_1 = require("./countries");
-function getCountries() {
-    return countries_1.list;
-}
-exports.getCountries = getCountries;
