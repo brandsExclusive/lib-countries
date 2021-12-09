f4adcbf64f5ef3eea44845321cce8756c239c555
jamesz96 <jamesz96.au@gmail.com>
2021-12-09 11:18:39 +1100

    Add new interface method

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ export function getCountries(): string[] {
   return Object.values(countries).map((c) => c.countryName)
 }
 
+export function getCountryObjs(): Country[] {
+  return Object.values(countries)
+}
+
 export function isoLookup(key: string): Country | undefined {
   return countries[key]
 }
